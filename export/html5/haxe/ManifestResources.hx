a53c6f730aa99c1aafdc95f0d58e6d1a8e142e7c
Joey <joeyprather8@gmail.com>
2019-08-25 06:58:23 -0700

    first

---
+++ export/html5/haxe/ManifestResources.hx
@@ -0,0 +1,127 @@
+package;
+
+
+import haxe.io.Bytes;
+import lime.utils.AssetBundle;
+import lime.utils.AssetLibrary;
+import lime.utils.AssetManifest;
+import lime.utils.Assets;
+
+#if sys
+import sys.FileSystem;
+#end
+
+@:access(lime.utils.Assets)
+
+
+@:keep @:dox(hide) class ManifestResources {
+
+
+	public static var preloadLibraries:Array<AssetLibrary>;
+	public static var preloadLibraryNames:Array<String>;
+	public static var rootPath:String;
+
+
+	public static function init (config:Dynamic):Void {
+
+		preloadLibraries = new Array ();
+		preloadLibraryNames = new Array ();
+
+		rootPath = null;
+
+		if (config != null && Reflect.hasField (config, "rootPath")) {
+
+			rootPath = Reflect.field (config, "rootPath");
+
+		}
+
+		if (rootPath == null) {
+
+			#if (ios || tvos || emscripten)
+			rootPath = "assets/";
+			#elseif console
+			rootPath = lime.system.System.applicationDirectory;
+			#else
+			rootPath = "./";
+			#end
+
+		}
+
+		Assets.defaultRootPath = rootPath;
+
+		#if (openfl && !flash && !display)
+		
+		#end
+
+		var data, manifest, library, bundle;
+
+		#if kha
+
+		null
+		library = AssetLibrary.fromManifest (manifest);
+		Assets.registerLibrary ("null", library);
+
+		if (library != null) preloadLibraries.push (library);
+		else preloadLibraryNames.push ("null");
+
+		#else
+
+		data = '{"name":null,"assets":"aoy4:pathy14:assets%2F0.pngy4:sizei132720y4:typey5:IMAGEy2:idR1goR0y14:assets%2F1.pngR2i68490R3R4R5R6goR0y14:assets%2F2.pngR2i405443R3R4R5R7goR0y14:assets%2F3.pngR2i578630R3R4R5R8gh","rootPath":"../","version":2,"libraryArgs":[],"libraryType":null}';
+		manifest = AssetManifest.parse (data, rootPath);
+		library = AssetLibrary.fromManifest (manifest);
+		Assets.registerLibrary ("default", library);
+		
+
+		library = Assets.getLibrary ("default");
+		if (library != null) preloadLibraries.push (library);
+		else preloadLibraryNames.push ("default");
+		
+
+		#end
+
+	}
+
+
+}
+
+
+#if kha
+
+null
+
+#else
+
+#if !display
+#if flash
+
+@:keep @:bind @:noCompletion #if display private #end class __ASSET__assets_0_png extends flash.display.BitmapData { public function new () { super (0, 0, true, 0); } }
+@:keep @:bind @:noCompletion #if display private #end class __ASSET__assets_1_png extends flash.display.BitmapData { public function new () { super (0, 0, true, 0); } }
+@:keep @:bind @:noCompletion #if display private #end class __ASSET__assets_2_png extends flash.display.BitmapData { public function new () { super (0, 0, true, 0); } }
+@:keep @:bind @:noCompletion #if display private #end class __ASSET__assets_3_png extends flash.display.BitmapData { public function new () { super (0, 0, true, 0); } }
+@:keep @:bind @:noCompletion #if display private #end class __ASSET__manifest_default_json extends null { }
+
+
+#elseif (desktop || cpp)
+
+@:keep @:file("") @:noCompletion #if display private #end class __ASSET__manifest_default_json extends haxe.io.Bytes {}
+
+
+
+#else
+
+
+
+#end
+
+#if (openfl && !flash)
+
+#if html5
+
+#else
+
+#end
+
+#end
+#end
+
+#end
---
+++ src/Main.hx
@@ -0,0 +1,108 @@
+package;
+
+import motion.easing.Cubic;
+import motion.easing.Back;
+import motion.easing.Elastic;
+import motion.easing.Quart;
+import motion.easing.Bounce;
+import motion.easing.Expo;
+import motion.easing.Quad;
+import motion.easing.Sine;
+import haxe.Timer;
+import openfl.display.Shape;
+import openfl.events.Event;
+import openfl.display.BitmapData;
+import openfl.Assets;
+import openfl.display.Bitmap;
+import openfl.display.Sprite;
+import motion.Actuate;
+
+class Main extends Sprite
+{
+	var image:Bitmap;
+	var index:Int = 0;
+	var max:Int = 3;
+	public function new()
+	{
+		super();
+		stage.color = 0x7f7f7f;
+		//stage.color = 0;
+		image = new Bitmap();
+		image.smoothing = true;
+		addChild(image);
+		change(0,gallery);
+		//events
+		stage.addEventListener(Event.RESIZE,resize);
+	}
+	private function gallery()
+	{
+		Actuate.tween(image,0.6,{alpha:0},false).onComplete(function(_)
+		{
+			change(++index,function()
+			{
+				trace("index " + index);
+				Actuate.tween(image,0.4,{alpha:1},false).onComplete(function(_)
+				{
+					gallery();
+				}).ease(Expo.easeOut);
+				switch (index)
+				{
+					case 0:
+					//baby
+					Actuate.tween(image,2,{x:image.x,y:image.y,width:image.width,height:image.height});
+					//image.x += -image.width * 0.5 * 0.5;
+					//image.y += -image.height * 0.5 * 0.5;
+					image.width *= 0.5;
+					image.height *= 0.5;
+					case 1:
+					//baker
+					Actuate.tween(image,2,{y:image.y}).ease(Bounce.easeOut);
+					image.y = -image.height;
+					case 2:
+					//michael
+					Actuate.tween(image,2.2,{y:image.y}).ease(Expo.easeOut);
+					image.y = stage.stageHeight;
+					case 3:
+					//sword lady
+					Actuate.tween(image,4,{x:image.x,rotation:0}).ease(Expo.easeOut);
+					image.x = stage.stageWidth;
+					image.rotation = 15;
+				}
+				if (index >= max) 
+				{
+					trace("max index");
+					index = -1;
+				}
+			});
+		}).delay(3 * 1).ease(Expo.easeOut);
+		
+	}
+	private function change(index:Int,finish:Void->Void)
+	{
+		Assets.loadBitmapData("assets/" + index + ".png").onComplete(function(bmd:BitmapData)
+		{
+			image.bitmapData = bmd;
+			image.width = image.bitmapData.width;
+			image.height = image.bitmapData.height;
+			resize(null);
+			finish();
+		});
+	}
+	private function resize(_)
+	{
+		//image
+		var ratio:Float = image.width/image.height;
+		//trace("ratio " + ratio);
+		//trace("stage w " + stage.stageWidth + " h " + stage.stageHeight);
+		//trace("width " + image.width + " height " + height);
+		image.height = stage.stageHeight * 1;
+		image.width = image.height * ratio;
+		trace("scale " + image.scaleX + " " + image.scaleY);
+		center();
+	}
+	private function center()
+	{
+		image.x = (stage.stageWidth - image.width)/2;
+		image.y = (stage.stageHeight - image.height)/2;
+	}
+}
